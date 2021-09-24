312e8ba752c8f2cdeb5f1493acecc8232ed22aa4
SongJunHa <reoiy12@naver.com>
2021-09-24 19:59:00 +0900

    Ground AI Server Modify

--- app.py
+++ app.py
@@ -31,70 +31,40 @@ def ai_ground():
     message = yolo3('worker_images/worker.jpg')
     print("-----------------------------")
     print(message)
+    print(request.form['kakaoid'])
     print("-----------------------------")
 
     result = message.split('\n')
     print(result)
 
-    db= pymysql.connect(host=os.environ.get("DB_host"),
-                port=int(os.environ.get("DB_port")),
-                user=os.environ.get("DB_user"),
-                passwd=os.environ.get("DB_password"),
-                db=os.environ.get("DB_database"),
-                charset='utf8')
+    # db= pymysql.connect(host=os.environ.get("DB_host"),
+    #             port=int(os.environ.get("DB_port")),
+    #             user=os.environ.get("DB_user"),
+    #             passwd=os.environ.get("DB_password"),
+    #             db=os.environ.get("DB_database"),
+    #             charset='utf8')
     
-    cursor = db.cursor()
+    # cursor = db.cursor()
 
-    for i in result :
-        if i == "no_goggles" :
-            print('1')
-        elif i == "no_vest" :
-            print("2")
-        elif i == "no_helmet" :
-            sql = "UPDATE Worker SET wEquipment = 'nohelmet' where wid = '1760690698'"
-        elif i == "no_gloves" :
-            print("4")
-        else :
-            print("5")
-    cursor.execute(sql)
-    db.commit()
-    db.close()
-
-    return result
-
-@app.route('/ai_post',methods=['GET','POST'])
-def post():
-        path_dir = "../tamjiat_web/public/upload"
-        file_list = os.listdir(path_dir)
-        name = ""
-        print(request.form['file_name'])
-        print(request.form['cduuid'])
-        
-        lists = request.form['file_name']
-        uuid = request.form['cduuid']
-
-        for i in file_list:
-            if i == lists:
-                name = i
-        result = AI_Test(path_dir+"/"+ name)
-        db= pymysql.connect(host=os.environ.get("DB_host"),
-                     port=int(os.environ.get("DB_port")),
-                     user=os.environ.get("DB_user"),
-                     passwd=os.environ.get("DB_password"),
-                     db=os.environ.get("DB_database"),
-                     charset='utf8')
-        cursor = db.cursor()
-        if result=="정상":
-            sql = "UPDATE userDcrop SET AICheck = '완료' , cdName = '"+result+"', iscdCheck='false' where cduuid = '"+uuid+"'"
-        else:
-            sql = "UPDATE userDcrop SET AICheck = '완료' , cdName = '"+result+"', iscdCheck='true' where cduuid = '"+uuid+"'"
-        cursor.execute(sql)
-        db.commit()
-        db.close()
-        return result
+    # for i in result :
+    #     if i == "no_goggles" :
+    #         print('1')
+    #     elif i == "no_vest" :
+    #         print("2")
+    #     elif i == "no_helmet" :
+    #         sql = "UPDATE Worker SET wEquipment = 'nohelmet' where wid = '1760690698'"
+    #     elif i == "no_gloves" :
+    #         print("4")
+    #     else :
+    #         print("5")
+    # cursor.execute(sql)
+    # db.commit()
+    # db.close()
+    
+    return request.form['kakaoid']
     
 
 if __name__ == '__main__':
     ssl_context= ssl.SSLContext(ssl.PROTOCOL_TLS)
-    ssl_context.load_cert_chain(certfile='server.crt',keyfile='private.key',password='qwerty12')
-    app.run(host='localhost',port=5000,debug=True,ssl_context=ssl_context)
+    ssl_context.load_cert_chain(certfile='private.crt',keyfile='private.key',password='qwerty12')
+    app.run(host='0.0.0.0',port=5000,debug=True,ssl_context=ssl_context)
